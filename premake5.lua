4c3c8763398bb994d6687ec7f2a0af5bfdb6e18b
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-09-16 16:04:22 +0200

    Added missing files from the build

--- premake5.lua
+++ premake5.lua
@@ -17,7 +17,8 @@ project "glfwPremake"
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/platform.c"
 	}
 
 	filter "system:linux"
@@ -53,6 +54,7 @@ project "glfwPremake"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
